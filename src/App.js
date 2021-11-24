4ad34a5f27f664208a3c11597d7287434fe6f86c
pechenyice <german.iskhakov@mail.ru>
2021-11-24 03:20:20 +0300

    refactor Menu to fixed with transition

--- src/App.js
+++ src/App.js
@@ -2,19 +2,16 @@ import React, { useState, useEffect, useRef } from "react";
 import ProjectsList from "./components/ProjectsList/ProjectsList";
 import Header from "./components/Header/Header";
 import Heading from "./components/Heading/Heading";
-import ServicesList from "./components/ServicesList/ServicesList";
+
 import HeaderBlock from "./components/HeaderBlock/HeaderBlock";
 import "./App.css";
 import Footer from "./components/Footer/Footer";
 import Menu from "./components/Menu/Menu";
+import { CSSTransition } from "react-transition-group";
 
 function App() {
 	let [reviews, setReviews] = useState({});
 	let [menu, setMenu] = useState(false);
-
-	let ref1 = useRef(null);
-	let ref2 = useRef(null);
-	let ref3 = useRef(null);
 
 	useEffect(async () => {
 		let response = await fetch("/getReviews", { method: "GET" });
@@ -36,7 +33,7 @@ function App() {
 
 	return (
 		<>
-			<div className="maxWidthWrapper">
+			<div className="maxWidthWrapperHeader">
 				<Header onMenuOpen={handleMenuLogic} />
 			</div>
 			<section className="sectionWrapper">
@@ -63,10 +60,17 @@ function App() {
 			</section>
 			<div className="backgroundFooter">
 				<div className="maxWidthWrapper">
-					<Menu />
 					<Footer />
 				</div>
 			</div>
+			<CSSTransition
+				in={menu}
+				timeout={300}
+				classNames="menuAppearence"
+				unmountOnExit
+			>
+				<Menu onClose={handleMenuLogic} />
+			</CSSTransition>
 		</>
 	);
 }
