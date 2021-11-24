ce1c8238e223b0ef78294f3e712e0e3a20f34d52
Pechenyice <german.iskhakov@mail.ru>
2021-11-24 20:13:38 +0300

    fixed menu alignment

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -1,7 +1,7 @@
-import react from "react";
-import "./Menu.css";
-import logoDark from "./../../assets/logo_dark.png";
-import ContactInfo from "../ContactInfo/ContactInfo";
+import react from 'react';
+import './Menu.css';
+import logoDark from './../../assets/logo_dark.png';
+import ContactInfo from '../ContactInfo/ContactInfo';
 
 const Menu = (props) => {
   return (
@@ -10,51 +10,51 @@ const Menu = (props) => {
         <div className="logoMenu">
           <img src={logoDark} onClick={props.onClose} />
         </div>
-		<div className="flexMenuContentCenter">
         <div className="flexMenuWrapper">
-          <div className="linksMenuWrapper">
-            <a className="linkMenu" href="#">
-              Главная
-            </a>
-            <a className="linkMenu" href="#">
-              Об агентстве
-            </a>
-            <a className="linkMenu" href="#">
-              Услуги
-            </a>
-            <a className="linkMenu" href="#">
-              Кейсы
-            </a>
-            <a className="linkMenu" href="#">
-              Блог
-            </a>
-            <a className="linkMenu" href="#">
-              Контакты
-            </a>
+          <div className="flexMenuInner">
+            <div className="linksMenuWrapper">
+              <a className="linkMenu" href="#">
+                Главная
+              </a>
+              <a className="linkMenu" href="#">
+                Об агентстве
+              </a>
+              <a className="linkMenu" href="#">
+                Услуги
+              </a>
+              <a className="linkMenu" href="#">
+                Кейсы
+              </a>
+              <a className="linkMenu" href="#">
+                Блог
+              </a>
+              <a className="linkMenu" href="#">
+                Контакты
+              </a>
+            </div>
+
+            <div className="contactMenuWrapper">
+              <ContactInfo
+                subtitle={<span>почта</span>}
+                link={'#'}
+                contact={<span>info@digitalsquad.ru</span>}
+              />
+
+              <ContactInfo
+                subtitle={<span>телефон</span>}
+                link={'#'}
+                contact={<span>8 800 123 45 67</span>}
+              />
+
+              <ContactInfo
+                subtitle={<span>instagram</span>}
+                link={'#'}
+                contact={<span>@dgtlsquad</span>}
+              />
+            </div>
           </div>
-
-          <div className="contactMenuWrapper">
-            <ContactInfo
-              subtitle={<span>почта</span>}
-              link={"#"}
-              contact={<span>info@digitalsquad.ru</span>}
-            />
-
-            <ContactInfo
-              subtitle={<span>телефон</span>}
-              link={"#"}
-              contact={<span>8 800 123 45 67</span>}
-            />
-
-            <ContactInfo
-              subtitle={<span>instagram</span>}
-              link={"#"}
-              contact={<span>@dgtlsquad</span>}
-            />
-          </div>
-		</div>
+        </div>
       </div>
-	  </div>
     </div>
   );
 };
