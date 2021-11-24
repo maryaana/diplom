853ebafa62c4c33f1ec3d7f29cb008e3352669e1
pechenyice <german.iskhakov@mail.ru>
2021-11-24 03:20:00 +0300

    added header onScroll resize

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,14 +1,27 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import logo from "./../../logo.png";
 import "./Header.css";
 
 const Header = (props) => {
+	let wrapper = useRef(null);
+	useEffect(() => {
+		window.addEventListener("scroll", headerResizer);
+	}, []);
+
+	function headerResizer() {
+		if (window.scrollY > 100) {
+			wrapper.current.style.padding = "15px 0";
+		} else {
+			wrapper.current.style.padding = "30px 0";
+		}
+	}
+
 	return (
-		<div className="wrapper">
+		<div className="wrapper" ref={wrapper}>
 			<div className="logo">
 				<img src={logo} />
 			</div>
-			<div className="menuButton">
+			<div className="menuButton" onClick={props.onMenuOpen}>
 				<div className="menuLine"></div>
 				<div className="menuLine"></div>
 			</div>
