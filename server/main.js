648466bda31c7c618249d355bf3f2c73ad556a20
Pechenyice <german.iskhakov@mail.ru>
2021-11-26 19:36:51 +0300

    added routes for handling GET news, bids, cases, casesTags, reviews

--- server/main.js
+++ server/main.js
@@ -4,21 +4,77 @@ const port = 3001;
 const db = require('./db.js');
 
 app.get('/getReviews', async (req, res) => {
-  let reviews = await db.checkTable();
+  let reviews = null;
+  try {
+    reviews = await db.getReviews();
+  } catch (e) {
+    console.log(e);
+    res.json({ success: false });
+  }
 
-  if (reviews == null) res.json({success: false});
+  if (reviews == null) res.json({ success: false });
 
-  res.json({success: true, data: reviews});
-})
+  setTimeout(() => {
+    res.json({ success: true, data: reviews });
+  }, 1000);
+});
 
-app.get('/getReviews', async (req, res) => {
-  let reviews = await db.checkTable();
+app.get('/getBids', async (req, res) => {
+  let bids = null;
+  try {
+    bids = await db.getBids();
+  } catch (e) {
+    console.log(e);
+    res.json({ success: false });
+  }
 
-  if (reviews == null) res.json({success: false});
+  if (bids == null) res.json({ success: false });
 
-  res.json({success: true, data: reviews});
-})
+  res.json({ success: true, data: bids });
+});
+
+app.get('/getNews', async (req, res) => {
+  let news = null;
+  try {
+    news = await db.getNews();
+  } catch (e) {
+    console.log(e);
+    res.json({ success: false });
+  }
+
+  if (news == null) res.json({ success: false });
+
+  res.json({ success: true, data: news });
+});
+
+app.get('/getCasesTags', async (req, res) => {
+  let casesTags = null;
+  try {
+    casesTags = await db.getCasesTags();
+  } catch (e) {
+    console.log(e);
+    res.json({ success: false });
+  }
+
+  if (casesTags == null) res.json({ success: false });
+
+  res.json({ success: true, data: casesTags });
+});
+
+app.get('/getCases', async (req, res) => {
+  let cases = null;
+  try {
+    cases = await db.getCases();
+  } catch (e) {
+    console.log(e);
+    res.json({ success: false });
+  }
+
+  if (cases == null) res.json({ success: false });
+
+  res.json({ success: true, data: cases });
+});
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
-})
+});
