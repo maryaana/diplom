45fd7bb09351a5fa83bffa3a421175c22139df44
Pechenyice <german.iskhakov@mail.ru>
2021-11-26 19:41:54 +0300

    refactored to server fetched data

--- src/components/LastReviewsGallery/LastReviewsGallery.js
+++ src/components/LastReviewsGallery/LastReviewsGallery.js
@@ -2,20 +2,15 @@ import React, { useState } from 'react';
 import { CSSTransition } from 'react-transition-group';
 import './LastReviewsGallery.css';
 
-const LastReviewsGallery = () => {
-  let [reviews, setReviews] = useState([
-    'Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. ',
-    'ure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. ',
-    'Биба и боба 2 долболба',
-  ]);
-
+const LastReviewsGallery = (props) => {
   let [index, setIndex] = useState(0);
+  let reviews = props.reviews?.slice(0, 4);
 
   function handleNewIndex(sign) {
     return () => {
       switch (sign) {
         case '+': {
-          if (index + 1 >= reviews.length) return;
+          if (index + 1 >= reviews?.length) return;
           setIndex(index + 1);
           return;
         }
@@ -37,20 +32,21 @@ const LastReviewsGallery = () => {
     <section className="lastReviewsGalleryWrapper">
       <section className="lastReviewsGalleryInner">
         <section className="lastReviewsGalleryInnerContent">
-          {reviews.map((r, i) => (
+          {reviews?.map((r, i) => (
             <CSSTransition
               in={i === index}
               timeout={500}
               classNames="lastReviewsGalleryInnerContentAppearence"
               unmountOnExit
+              key={i}
             >
-              <div>{r}</div>
+              <div>{r.text}</div>
             </CSSTransition>
           ))}
         </section>
         <section className="lastReviewsGalleryInnerControls">
           <div className="lastReviewsControlDisplaysWrapper">
-            {reviews.map((e, i) => (
+            {reviews?.map((e, i) => (
               <div
                 key={i}
                 className={[
@@ -73,7 +69,7 @@ const LastReviewsGallery = () => {
               <path
                 d="M7.875 1L2 7.27586M2 7.27586L8.29464 14M2 7.27586H49"
                 stroke={index <= 0 ? '#A0BFD7' : '#009FD7'}
-                stroke-width="2"
+                strokeWidth="2"
               />
             </svg>
 
@@ -87,8 +83,8 @@ const LastReviewsGallery = () => {
             >
               <path
                 d="M41.125 1L47 7.27586M47 7.27586L40.7054 14M47 7.27586H0"
-                stroke={index >= reviews.length - 1 ? '#A0BFD7' : '#009FD7'}
-                stroke-width="2"
+                stroke={index >= reviews?.length - 1 ? '#A0BFD7' : '#009FD7'}
+                strokeWidth="2"
               />
             </svg>
           </div>
