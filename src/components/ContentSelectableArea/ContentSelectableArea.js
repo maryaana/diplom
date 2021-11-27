1e07ada41f30c606bfd6ef196034047d2cc03a2a
Pechenyice <german.iskhakov@mail.ru>
2021-11-27 03:24:09 +0300

    created catalog main area

---
+++ src/components/ContentSelectableArea/ContentSelectableArea.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { useParams } from 'react-router-dom';
+import { SelectableContentDisplay, SelectableTagsDisplay } from '..';
+import './ContentSelectableArea.css';
+
+const ContentSelectableArea = (props) => {
+  const params = useParams();
+
+  console.log(params);
+  return (
+    <section>
+      <SelectableTagsDisplay
+        type={props.type}
+        content={props.content}
+        tags={props.tags}
+        target={params.tag}
+        base={props.base}
+      />
+      <SelectableContentDisplay
+        type={props.type}
+        base={props.base}
+        content={props.content}
+        contentMount={props.contentMount}
+        target={params.tag}
+      />
+    </section>
+  );
+};
+
+export default ContentSelectableArea;
