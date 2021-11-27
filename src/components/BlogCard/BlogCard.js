ab632f7f432c2b5b8fb56559ef82d355841b2ecd
Pechenyice <german.iskhakov@mail.ru>
2021-11-27 03:23:07 +0300

    added links

--- src/components/BlogCard/BlogCard.js
+++ src/components/BlogCard/BlogCard.js
@@ -1,25 +1,24 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './BlogCard.css';
 
 const BlogCard = (props) => {
   let base = '/news/';
 
   return (
-    <>
-      {
-        <div className="blogCardContainer">
-          <div className="photoBlogCard">
-            <img src={`${base}${props.photo}`} />
-          </div>
-
-          <div className="categoryDateBlogCardWrapper">
-            <div className="categotyBlogCard">{props.categoty}</div>
-            <div className="creationDateBlogCard">{props.date}</div>
-          </div>
-          <div className="titleBlogCard">{props.title}</div>
+    <Link to={props.link}>
+      <div className="blogCardContainer">
+        <div className="photoBlogCard">
+          <img src={`${base}${props.photo}`} />
         </div>
-      }
-    </>
+
+        <div className="categoryDateBlogCardWrapper">
+          <div className="categotyBlogCard">{props.categoty}</div>
+          <div className="creationDateBlogCard">{props.date}</div>
+        </div>
+        <div className="titleBlogCard">{props.title}</div>
+      </div>
+    </Link>
   );
 };
 
